16893e68c41dda38b42f47053ae7e7d4cda0272e
Matthew Carlson <matthewfcarlson@users.noreply.github.com>
2024-06-30 20:58:26 -0500

    Add initial firmware and platformio config

---
+++ firmware/src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include <esp_system.h>
+#include <esp_now.h>
+#include <WiFi.h>
+#include <esp_wifi.h>
+#include <cstring>
+#include <list>
+
+void setup() {
+    // TODO: setup
+}
+
+void loop() {
+    // TODO: loop
+}
